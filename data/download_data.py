c6e0c9973270c3674a87f9636798bfd1cf5a7893
Xiu <>
2025-09-20 16:52:21 -0700

    download dataset, sampling

--- data/download_data.py
+++ data/download_data.py
@@ -1,17 +1,17 @@
 import os
 import torchvision
 from utils import config_util as cfg_util
+from datasets import load_dataset
 
-def download(config):
-    timestamp = cfg_util.get_timestamp()
-    setname= cfg["active_dataset"]
-    ds_cfg = cfg["datasets"][setname]
+def get_datasets(config):
+    setname= config["active_dataset"]
+    ds_cfg = config["datasets"][setname]
 
     # destination
-    des_path = cfg["path"]["root"]
+    des_path = config["path"]["root"]
     os.makedirs(des_path , exist_ok=True)
 
-    # 
+    # plan change: SKIP STL-10
     if setname == "stl10":
         trainset = torchvision.datasets.STL10(
             root=des_path,
@@ -23,28 +23,32 @@ def download(config):
             split=ds_cfg["val_split"],
             download=True
         ) 
-    elif setname == "cifar10":
-        trainset = torchvision.datasets.CIFAR10(
-            root=des_path,
-            train=True,
-            download=True
-        )
-        valset = torchvision.datasets.CIFAR10(
-            root=des_path,
-            train=False,
-            download=True
-        )
-    elif setname == "imagenet100":
-        # ⚠️ 
-        print("[WARN] ImageNet-100 is not included in torchvision, it needs to be downloaded manually.")
-        return
-    
-    print(f"[INFO] {setname} dataset downloaded to {des_path}")
+
+    elif setname == "cline9":
+        # hugging face Clane9/Imagenet100
+        dataset = load_dataset("clane9/imagenet-100", cache_dir=des_path)
+
+        trainset = sample_dataset(dataset[ds_cfg["train_split"]],
+                                  ds_cfg.get("train_size"),
+                                  cfg["sampling"]["seed"])
+        valset = sample_dataset(dataset[ds_cfg["val_split"]],
+                                ds_cfg.get("val_size"),
+                                cfg["sampling"]["seed"])
+        
+    print(f"[INFO] {setname} dataset ready at {des_path}")
     print(f"[INFO] Train size: {len(trainset)}, Val size: {len(valset)}")
+
+    return trainset, valset
+
+# Hugging Face, select a subset
+def sample_dataset(dataset, size, seed=42):
+    if size is None or size > len(dataset):
+        return dataset
+    return dataset.shuffle(seed=seed).select(range(size))
 
 if __name__ == "__main__":
     # load config
     cfg = cfg_util.load_yaml('./config/dataset_config.yaml')
 
     # download data
-    download(cfg)
+    get_datasets(cfg)
---
+++ src/datasets/dataset_loader.py
@@ -0,0 +1,4 @@
+from utils import load_yaml as cfg_util
+from data.dataset_loader import get_datasets
+from torch.util.data import DataLoader
+
