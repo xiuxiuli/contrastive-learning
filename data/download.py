ccb24d0cee7578d605bfed0756a35dd908046ca2
Xiu <>
2025-10-27 20:50:14 -0700

    update download yaml with omega

--- data/download.py
+++ data/download.py
@@ -1,28 +1,33 @@
 import sys, os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-from utils import tool
 from datasets import load_dataset
+from omegaconf import OmegaConf
+from pathlib import Path
+
+PROJECT_ROOT = Path(__file__).resolve().parent.parent
+os.chdir(PROJECT_ROOT) 
 
 def download(cfg):
     
-    data_cfg = cfg["dataset"]
-    data_name = data_cfg["name"]
+    data_cfg = cfg.dataset
+    data_name = data_cfg.name
 
     # destination
-    root_dir = tool.get_root_dir(cfg)
-    output_dir = os.path.join(root_dir, data_cfg["output_subdir"])
+    # root_dir = tool.get_root_dir(cfg)
+    # output_dir = os.path.join(root_dir, data_cfg["output_subdir"])
+    output_dir = Path(data_cfg.output_subdir)
     os.makedirs(output_dir, exist_ok=True)
 
     data = load_dataset(data_name, cache_dir=output_dir)
 
-    splits = data_cfg["splits"]
-    sizes = data_cfg["sizes"]
-    seed =  cfg["sampling"]["seed"]
+    splits = data_cfg.split
+    sizes = data_cfg.sizes
+    seed =  cfg.sampling.seed
 
-    trainset = sample_dataset(data[splits["train"]], sizes["train"], seed)
-    valset = sample_dataset(data[splits["val"]], sizes["val"], seed)
-    testset = sample_dataset(data[splits["test"]], sizes["test"], seed)
+    trainset = sample_dataset(data[splits.train], sizes.train, seed)
+    valset = sample_dataset(data[splits.val], sizes.val, seed)
+    testset = sample_dataset(data[splits.test], sizes.test, seed)
    
     print(f"[INFO] {data_name} downloaded and cached to {output_dir}")
     print(f"[INFO] Train: {len(trainset)}, Val: {len(valset)}, Test: {len(testset)}")
@@ -37,7 +42,8 @@ def sample_dataset(dataset, size, seed=42):
 
 if __name__ == "__main__":
     # load config
-    cfg = tool.load_yaml('./config/data_config.yaml', True)
+    cfg = OmegaConf.load('./configs/data_config.yaml')
+    # cfg = tool.load_yaml('./config/data_config.yaml', True)
 
     # download data
     download(cfg)
--- utils/colab_setup.py
+++ utils/colab_setup.py
@@ -3,8 +3,8 @@
 # Usage: copy-paste into first cell in Colab notebook
 # ================================
 
-from google.colab import drive
-drive.mount('/content/drive')
+# from google.colab import drive
+# drive.mount('/content/drive')
 
 # %cd /content/drive/MyDrive/contrastive-learning-project/
 
